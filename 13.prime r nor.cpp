13678f7a2519be2addc43465234720b26df170bc
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2019-08-07 00:31:08 +0530

    Update 13.prime r nor.cpp

--- 13.prime r nor.cpp
+++ 13.prime r nor.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -19,6 +20,31 @@ int main(){
             cout<<"no";
         }
     }
+    */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, flag = 1;
+    cin >> num;
+    
+    for(int divisor = 2; divisor <= n/2; divisor++)
+    {
+        if(n%divisor == 0)
+        {
+            flag = 0;
+            break;
+        }
+    }
+    
+    if(flag)
+        cout << "yes";
+    else
+        cout << "no";
+    
+    return 0;
+}
     
     
 
