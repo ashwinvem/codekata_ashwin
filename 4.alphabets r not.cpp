8f09f1886ec4b946d8127a02926d15f9dca167af
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2019-08-06 21:15:26 +0530

    Create 4.alphabets r not.cpp

---
+++ 4.alphabets r not.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+char ch;
+int main(){
+    cout<<"alphabet";
+    cin>>ch;
+    if(isalpha(ch))
+    cout<<"s it's";
+    else
+    cout<<"no it's not";
+    
+    return 0;
+}
