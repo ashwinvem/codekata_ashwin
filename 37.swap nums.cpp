8a3423cb6775c02a303ee0d766958682f7106958
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2019-08-06 23:09:11 +0530

    Create 37.swap nums.cpp

---
+++ 37.swap nums.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  int num1, num2;
+  
+  cin >> num1 >> num2;
+  
+  num1 = num1 + num2;
+  num2 = num1 - num2;
+  num1 = num1 - num2;
+  
+  cout << num1 << " " << num2;
+  
+  return 0;
+}
