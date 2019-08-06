f3ca595f9283a53c687a20be4bfd4f454197f487
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2019-08-06 23:43:04 +0530

    Update 3.vowel r not.cpp

--- 3.vowel r not.cpp
+++ 3.vowel r not.cpp
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 using namespace std;
 
@@ -15,5 +16,44 @@ int main(){
     cout<<"enter the char";
     else
     cout<<"is a spc char";
+    return 0;
+}
+*/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+    string vowel = "aeiou";
+    int flag = 0;
+    char input;
+    
+    cin >> input;
+    if ((input >= 65 && input <= 90) || (input >= 97 && input <= 122))
+    {
+        if(input >= 65 && input <= 90)
+        {
+            input += 32;
+        }
+        for(int ind = 0; vowel[ind] != '\0'; ind++)
+        {
+            if(vowel[ind] == input)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if(flag)
+        {
+            cout << "Vowel";
+        }
+        else{
+            cout << "Consonant";
+        }
+    }
+    else{
+        cout << "invalid";
+    }
     return 0;
 }
