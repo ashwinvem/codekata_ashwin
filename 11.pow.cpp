c092b81fe985ce3c9f8fca5c65eb5db693e6a8ea
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2019-08-06 21:23:55 +0530

    Create 11.pow.cpp

---
+++ 11.pow.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int a;
+    float n,k;
+    cout<<"nter the valuses";
+    cin>>n>>k;
+    a=pow(n,k);
+    cout<<a;
+}
