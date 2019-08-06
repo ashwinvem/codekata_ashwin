6c4147bb90347fb85834cfe9b8d2cdc04f22d7d7
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2019-08-06 21:35:03 +0530

    Create 14.odd bw 2 intvls.cpp

---
+++ 14.odd bw 2 intvls.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num,min,max;
+    cout<<"max pls";
+    cin>>max;
+    cout<<"min pls";
+    cin>>min;
+    for(num=min+1;num<max;num++){
+        if(num%2 != 0)
+            cout<<num<<' ';
+    }
+    return 0;
+}
