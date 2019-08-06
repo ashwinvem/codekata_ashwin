ab20d33e6ffd34d47c3bef1ab9114e5de1098c5b
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2019-08-07 01:03:41 +0530

    Update 22.large ele in arr.cpp

--- 22.large ele in arr.cpp
+++ 22.large ele in arr.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main ()
 {
     int arr[10], n, i, max;
-    cout << "Enter the size of the array : ";
+    
     cin >> n;
-    cout << "Enter the elements of the array : ";
+    
     for (i = 0; i < n; i++)
         cin >> arr[i];
     max = arr[0];
@@ -14,5 +14,5 @@ int main ()
         if (max < arr[i])
             max = arr[i];
     }
-    cout << "Largest element : " << max;
+    cout << max;
 }    
