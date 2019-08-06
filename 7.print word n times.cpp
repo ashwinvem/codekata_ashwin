2356a4f900caa7241bdc38dc708caa0d832481c0
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2019-08-07 00:01:45 +0530

    Update 7.print word n times.cpp

--- 7.print word n times.cpp
+++ 7.print word n times.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main(){
     int i, n;
-    cout<<"enter the num";
+    
     cin>>n;
     for( i=0; i<n; i++)
-    cout<<"hlo";
+    cout<<"Hello";
     
     return 0;
 }
