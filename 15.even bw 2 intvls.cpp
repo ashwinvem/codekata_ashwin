de8ebe724b0e4409e75ea1fe57975f645c8d834e
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2019-08-07 00:41:07 +0530

    Update 15.even bw 2 intvls.cpp

--- 15.even bw 2 intvls.cpp
+++ 15.even bw 2 intvls.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main(){
     int num,min,max;
-    cout<<"max pls";
+    
     cin>>max;
-    cout<<"min pls";
+    
     cin>>min;
     for(num=min+1;num<max;num++){
         if(num%2 == 0)
