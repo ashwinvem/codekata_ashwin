96a2554f881783e5a7c217607ddb84abbc92ae0f
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2019-08-06 21:18:50 +0530

    Create 6.leap r not.cpp

---
+++ 6.leap r not.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+float n1,n2,n3;
+int main(){
+    cout<<"enter 3 nums";
+    cin>>n1>>n2>>n3;
+    if(n1>=n2 && n1>=n3)
+    cout<<n1;
+    if(n2>=n1 && n2>=n3)
+    cout<<n2;
+    if(n3>=n2 && n3>=n1)
+    cout<<n3;
+    
+    return 0;
+    
+}
