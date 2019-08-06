aa45dd827980b67b1bb523872167b17d6c49f1cd
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2019-08-06 21:14:07 +0530

    Create 2.even r odd.cpp

---
+++ 2.even r odd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int num;
+int main(){
+    cout<<"number";
+    cin>>num;
+    if(num%2==0){
+        cout<<"even";
+    }
+    else{
+        cout<<"odd";
+    }
+    return 0;
+}
